61db78a8215255efe7af1bbf3b99a1bfc7daf21e
kariukibetty <user.name>
2022-09-08 13:58:59 -0700

    programming4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,22 +1,10 @@
-#!/bin/bash
 #include <stdio.h>
-
 /**
- *
- *  *main - entry point
- *
- *   *
- *
- *    *Return: 0 after printing the function
- *
- *     */
-
+ *main -entry 
+ *Return: 0 after printing the function
+ */
 int main(void)
-
 {
-
-	puts("\"Programming is like building a multilingual puzzle");
-
-	return (0);
-
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
