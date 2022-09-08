c70ea65fc1054ece0611c9209c8d0655666512e1
kariukibetty <user.name>
2022-09-08 14:35:15 -0700

    programming5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - Entry point
+*
+* Return: Always 0 (success)
+*/
+int main(void)
+{
+printf("wish proper grammar, but the outcome is piece of art,/n");
+return (0);
+}
