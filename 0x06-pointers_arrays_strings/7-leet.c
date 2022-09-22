02b06b5556dd59cef0c28d32bd6dfd31ec65df84
kariukibetty <user.name>
2022-09-22 11:03:15 -0700

    initial commit

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,24 +1,29 @@
 #include "main.h"
-
 /**
- *leet - converts a string into leet speak
- *@targ: the string to be converted into leet speak
- *Return: a string's pointer
+ *leet - encodes a string
+ *@str: the string to encode
+ *
+ *Return: the encode string
  */
-char *leet(char *targ)
+char *leet(char *str)
 {
-int h = 0, num[5] = {4, 3, 0, 1, 7}, q = 0;
-char alpha[10] = "AaEeOoLlTt";
+int i, j;
+char c[] = "aAeEoOtTlL";
+char n[] = "4433007711";
 
-for (; targ[h]; h++)
+i = 0;
+while (str[i] != '\0')
 {
-for (q = 0; (targ[h] != alpha[q] && q < 10); q++)
-;
-if (targ[x] == alpha[y])
+j = 0;
+while (c[j] != '\0')
 {
-q /= 2;
-targ[h] = (num[q] + '0');
+if (str[i] == c[j])
+{
+str[i] = n[j];
 }
+j++;
 }
-return (targ);
+i++;
+}
+return (str);
 }
