4cb748203018aa8d48a4e75b62bec5e66337bb0b
kariukibetty <user.name>
2022-11-01 13:12:48 -0700

    manipulation

---
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * get_bit - Entry Point
+ * @n: input
+ * @index: index
+ * Return: 0
+ */
+
+int get_bit(unsigned long int n, unsigned int index)
+{
+{
+unsigned long int max = 0x01;
+
+max <<= index;
+if (max == 0)
+return (-1);
+
+if ((n & max))
+return (1);
+else
+return (0);
+}
+}
